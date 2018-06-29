94e3d94738e19c2515c9721895da8c412d4198ce
Phuc-Dao <tidaotidao@yahoo.com>
2018-06-29 10:11:52 -0700

    added candlestick tracker for xmr

--- live.py
+++ live.py
@@ -5,3 +5,28 @@ from botchart import BotChart
 from botstrategy import BotStrategy
 from botlog import BotLog
 from botcandlestick import BotCandlestick
+
+def main(argv):
+	chart = BotChart("poloniex","BTC_XMR",300,False)
+
+	strategy = BotStrategy()
+
+	candlesticks = []
+	developingCandlestick = BotCandlestick()
+
+	while True:
+		try:
+			developingCandlestick.tick(chart.getCurrentPrice())
+		except urllib2.URLError:
+			time.sleep(int(30))
+			developingCandlestick.tick(chart.getCurrentPrice())
+
+		if (developingCandlestick.isClosed()):
+			candlesticks.append(developingCandlestick)
+			strategy.tick(developingCandlestick)
+			developingCandlestick = BotCandlestick()
+		
+		time.sleep(int(30))
+
+if __name__ == "__main__":
+	main(sys.argv[1:])
