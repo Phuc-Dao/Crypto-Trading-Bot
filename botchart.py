2e5abc7118302686b163e6da181cefefa10f8ffb
Phuc-Dao <tidaotidao@yahoo.com>
2018-07-08 20:32:11 -0700

    constructor

--- botchart.py
+++ botchart.py
@@ -3,3 +3,12 @@ import urllib, json
 import pprint
 from botcandlestick import BotCandlestick
 
+class BotChart(object):
+	def __init__(self, exchange, pair, period, backtest=True):
+		self.pair = pair
+		self.period = period
+
+		self.startTime = 1491048000
+		self.endTime = 1491591200
+
+		self.data = []
